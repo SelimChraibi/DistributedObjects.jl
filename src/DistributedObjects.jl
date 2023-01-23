35175bb74f6edabe5fe29a2a5ea11c84e9c0b127
Selim <selimsepthuit@gmail.com>
2023-01-23 14:59:22 +0100

    Better doc

--- src/DistributedObjects.jl
+++ src/DistributedObjects.jl
@@ -24,30 +24,36 @@ module DistributedObjects
     #            DistributedObject
     ############################################
     
+    """
+        DistributedObject{T}() where T
+
+    Create an empty `DistributedObject` which will reference objects of type `T` stored on remote processes.
+    
+    For example, `DistributedObject{Int64}()`, will return a distributed object capable of referencing `Int64` objects.
+    
+        DistributedObject{T}(f::Function; pids=workers()::Vector{Int64})
+
+    Make a reference to objects of type `T` stored on processes `pids`.
+    `f` is a function that when executed on a `pid` in `pids` must return an implementation of an object of type `T`.
+    The default `pids` are the worker processes.
+    
+    For example, `DistributedObject((pid)->pid * ones(2); pids=[2,3])`, will return a distributed object referencing a vector `[2,2]` and `[3,3]` stored on workers 2 and 3 respectively.
+    
+        DistributedObject{T}(f::Function, pid::Int64)
+
+    Make a reference to an object of type `T` stored on process `pid`.
+    `f` is a function that when executed on `pid` must return an implementation of an object of type `T`.
+
+    For example, `DistributedObject(()->ones(2), 4)`, will return a distributed object referencing a vector `[1,1]` stored on workers 4.
+    """
     struct DistributedObject{T}
         refs::Dict{Int64,UUID}
-        """
-            DistributedObject{T}() where T
-
-        Create an empty `DistributedObject` which will reference objects of type `T` stored on remote processes.
-
-        For example, `DistributedObject{Int64}()`, will return a distributed object capable of referencing `Int64` objects.
-        """
+        
         DistributedObject{T}() where T = new(Dict{Int64,UUID}())
         
-        """
-            DistributedObject{T}(f::Function; pids=workers()::Vector{Int64})
-
-        Make a reference to objects of type `T` stored on processes `pids`.
-        `f` is a function that when executed on a `pid` in `pids` must return an implementation of an object of type `T`.
-        The default `pids` are the worker processes.
-
-        For example, `DistributedObject((pid)->pid * ones(2); pids=[2,3])`, will return a distributed object referencing a vector `[2,2]` and `[3,3]` stored on workers 2 and 3 respectively.
-        """
         function DistributedObject(f::Function; pids=workers()::Vector{Int64})
             @assert !isempty(pids) "Please make sure worker process have been launched and provide a non empty `pids`"
             refs = Dict{Int64,UUID}()
-
 
             types = Vector{Type}(undef, length(pids))
             @sync for (i,pid) in enumerate(pids)
@@ -57,14 +63,6 @@ module DistributedObjects
             new{types[1]}(refs)
         end
 
-        """
-            DistributedObject{T}(f::Function, pid::Int64)
-
-        Make a reference to an object of type `T` stored on process `pid`.
-        `f` is a function that when executed on `pid` must return an implementation of an object of type `T`.
-
-        For example, `DistributedObject(()->ones(2), 4)`, will return a distributed object referencing a vector `[1,1]` stored on workers 4.
-        """
         DistributedObject(f::Function, pid::Int64) = DistributedObject((pid)->f(), pids=[pid])
     end
 
